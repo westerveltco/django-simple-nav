72cc57ddea024536f691e60d4480caa1cc20a674
Josh <josh@joshthomas.dev>
2023-11-03 11:46:23 -0500

    add simple conf tests for coverage

---
+++ tests/test_conf.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import pytest
+from django.conf import settings
+
+from simple_nav.conf import SIMPLE_NAV_SETTINGS_NAME
+from simple_nav.conf import app_settings
+
+
+def test_default_settings():
+    user_settings = getattr(settings, SIMPLE_NAV_SETTINGS_NAME, {})
+
+    assert user_settings == {}
+
+
+def test_app_settings():
+    # temporary until app actually has settings
+    with pytest.raises(AttributeError):
+        app_settings.FOO
