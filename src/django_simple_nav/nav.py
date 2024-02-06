7813ba2b524eb3ec8f6cf4503a22048c24887c45
Josh <josh@joshthomas.dev>
2024-02-06 01:19:18 -0600

    change to resolved url to href in template context

--- src/django_simple_nav/nav.py
+++ src/django_simple_nav/nav.py
@@ -25,11 +25,11 @@ class Nav:
         ]
         for item in items:
             if item.url:
-                item.url = item.get_url()
+                item.href = item.get_href()
                 item.active = item.is_active(request)
             if hasattr(item, "items"):
                 for sub_item in item.items:
-                    sub_item.url = sub_item.get_url()
+                    sub_item.href = sub_item.get_href()
                     item.active = item.is_active(request)
         return render_to_string(
             template_name=cls.template_name,
@@ -43,12 +43,13 @@ class NavGroup:
     items: list[NavGroup | NavItem]
     url: str | None = None
     permissions: list[str] = field(default_factory=list)
+    href: str | None = None
     active: bool | None = None
 
-    def get_url(self) -> str | None:
+    def get_href(self) -> str | None:
         if not self.url:
             return None
-        return _get_url(self.url)
+        return _get_href(self.url)
 
     def is_active(self, request: HttpRequest) -> bool | None:
         if not self.url:
@@ -61,21 +62,22 @@ class NavItem:
     title: str
     url: str
     permissions: list[str] = field(default_factory=list)
+    href: str | None = None
     active: bool | None = None
 
-    def get_url(self) -> str:
-        return _get_url(self.url)
+    def get_href(self) -> str:
+        return _get_href(self.url)
 
     def is_active(self, request: HttpRequest) -> bool:
         return _check_item_active(request, self.url)
 
 
-def _get_url(url: str) -> str:
+def _get_href(url: str) -> str:
     try:
-        url = reverse(url)
+        href = reverse(url)
     except NoReverseMatch:
-        url = url
-    return url
+        href = url
+    return href
 
 
 def _check_item_active(request: HttpRequest, url: str) -> bool:
