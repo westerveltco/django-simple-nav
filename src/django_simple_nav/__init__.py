03332f002b15360359b8d2c826bb7f5a6af41372
Josh <josh@joshthomas.dev>
2024-02-23 12:01:19 -0600

    :bookmark: bump version 0.2.0 -> 0.3.0

--- src/django_simple_nav/__init__.py
+++ src/django_simple_nav/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __template_version__ = "2024.13"
--- tests/test_version.py
+++ tests/test_version.py
@@ -4,4 +4,4 @@ from django_simple_nav import __version__
 
 
 def test_version():
-    assert __version__ == "0.2.0"
+    assert __version__ == "0.3.0"
