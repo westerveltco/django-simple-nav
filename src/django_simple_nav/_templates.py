8a37fc3a671a6158d3b0c3dab10a4ffc05dbfeba
Josh <josh@joshthomas.dev>
2024-07-15 16:13:02 -0500

    get from first engine from settings

--- src/django_simple_nav/_templates.py
+++ src/django_simple_nav/_templates.py
@@ -6,6 +6,7 @@ from typing import Protocol
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpRequest
 from django.template import engines
+from django.template.backends.base import BaseEngine
 from django.template.context import Context
 from django.utils.safestring import SafeString
 
@@ -23,17 +24,30 @@ class EngineTemplate(Protocol):
 
 
 def from_string(template_code: str, using: str | None = None) -> EngineTemplate:
+    engine = get_engine(using)
+    return engine.from_string(template_code)
+
+
+def get_engine(using: str | None = None) -> BaseEngine:
     if app_settings.TEMPLATE_BACKEND is None:
-        try:
-            engine = engines.all()[0] if using is None else engines[using]
-        except IndexError as err:
-            msg = "No `BACKEND` found for a template engine. Please configure at least one in your TEMPLATES setting or set DJANGO_SIMPLE_NAV['TEMPLATE_BACKEND']"
-            raise ImproperlyConfigured(msg) from err
+        num_of_engines = len(engines.all())
+
+        if num_of_engines == 0:
+            msg = "No `BACKEND` found for a template engine. Please configure at least one in your `TEMPLATES` setting or set `DJANGO_SIMPLE_NAV['TEMPLATE_BACKEND']`."
+            raise ImproperlyConfigured(msg)
+
+        if num_of_engines > 1:
+            msg = "Multiple `BACKEND`s found for a template engine. Will proceed with first defined in list, otherwise set `DJANGO_SIMPLE_NAV['TEMPLATE_BACKEND']` to specify which one to use."
+            logger.warning(msg)
+
+        engine = engines.all()[0] if using is None else engines[using]
     else:
         try:
             backend_alias = app_settings.TEMPLATE_BACKEND.rsplit(".", 2)[-2]
         except Exception as err:
             msg = f"Invalid TEMPLATE_BACKEND for a template engine: {app_settings.TEMPLATE_BACKEND}. Check your DJANGO_SIMPLE_NAV['TEMPLATE_BACKEND'] setting."
             raise ImproperlyConfigured(msg) from err
+
         engine = engines[backend_alias]
-    return engine.from_string(template_code)
+
+    return engine
